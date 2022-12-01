66936b611f716885b82ab6ff0c0d468f07353de2
Xavier <52470412+xvrbnv@users.noreply.github.com>
2022-12-01 11:22:13 -0600

    Update python_api_test.py

--- python_api_test.py
+++ python_api_test.py
@@ -1,4 +1,4 @@
 import requests
 r = requests.get('http://0.0.0.0:9876/talkingparrot')
-print('Python API Test Status Code:' + (r.status_code))
-print('Python API Test Get Request Response Text:' + (r.text))
+print('Python API Test Status Code: ' + str(r.status_code))
+print('Python API Test Get Request Response Text: ' + (r.text))
